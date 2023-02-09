81734bd6324c565435ad9dbb94e10761d56025ef
rachelumwari <umwarirachel13@gmail.com>
2023-02-09 20:50:05 +0200

    test

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,122 +1,146 @@
-import USER from "../models/User";
-import { Request, Response } from "express"
-import { Twilio } from "twilio";
-const account_sid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const service_sid = process.env.TWILIO_SERVICE_SID;
+import USER from '../models/User'
 
-import { config } from 'dotenv'
+import {Request, Response} from 'express'
+import {Twilio} from 'twilio'
+import {encode} from '../helper/jwtTokenize'
+
+import {config} from 'dotenv'
 config()
+
+const account_sid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const service_sid = process.env.TWILIO_SERVICE_SID
 
 /* this class hold functions for authentication */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 class auth {
+  
+  static sendCode(req: Request, res: Response) {
+    const userPhone: string = req.params.phone
+    if (account_sid && authToken && service_sid) {
+      const Client = new Twilio(account_sid, authToken)
+      Client.verify.v2
+        .services(service_sid)
+        .verifications.create({to: userPhone, channel: 'sms'})
+        .then((resp) => {
+          res.status(200).json({message: 'Success sent', resp})
+        })
+        .catch((err) => {
+          res.status(400).json(err)
+        })
+    } else {
+      console.log('Please fill all vals')
+    }
+  }
+  static verify2FA(req: Request, res: Response) {
+    const userPhone: string = req.params.phone
+    const userCode: string = req.params.code
+    if (account_sid && authToken && service_sid) {
+      const Client = new Twilio(account_sid, authToken)
+      Client.verify.v2
+        .services(service_sid)
+        .verificationChecks.create({to: userPhone, code: userCode})
+        .then((resp) => {
+          res.status(200).json({message: 'Success sent', resp})
+        })
+        .catch((err) => {
+          res.status(400).json(err)
+        })
+    } else {
+      console.log('Please fill all vals')
+    }
+  }
+
+  // LOGOUT
+  static async logout(req: Request, res: Response) {
+    try {
+      res.cookie('jwt', '', {httpOnly: true, maxAge: 1000})
+      res.status(200).json({status: 200, message: 'Logged out'})
+    } catch (error: any) {
+      res.status(400).json({
+        statusCode: 400,
+        message: error.message,
+      })
+    }
+  }
     static async signup(req: Request, res: Response) {
-        //defined the type of user to be created 
-        type newuser = {
-            firstName: string,
-            lastName: string,
-            email: string,
-            password: string,
-        }
-        const userReq: newuser = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-        }
         try {
-            const user = await USER.create(userReq)
-            if (user) {
-                res.status(200).json({
-                    statusCode: 200,
-                    message: "created account succesfully"
-                })
-            }
-            // else{
-            //     res.status(400).json({
-            //         statusCode: 200,
-            //         message: "Failed to create user",                  
-            //     })                    
-            // }
-        }
-        catch (error: any) {
-            res.status(400).json({
-                statusCode: 400,
-                message: "Failed to create user",
-                error: error.message
-            })
+        // await USER.drop()
+      const {firstName, lastName, email, role, password} = req.body
+      //   const hash = await bcrypt.hashSync(password, 10)
+      const checkUser = await USER.findOne({
+        where: {email: email},
+      })
+      if (checkUser) {
+        return res.status(400).json({
+          status: 400,
+          message: 'User is already SignUp',
+        })
+      } else {
+        type userType = {
+          id: string
+          firstName: string
+          lastName: string
+          email: string
+          password: string
         }
 
+        const createData: any = await USER.create({
+          firstName,
+          lastName,
+          email,
+          role: 'User',
+          password,
+        })
+        const user = await USER.findOne({
+          where: {email: email},
+          attributes: ['id', 'firstName', 'lastName', 'email', 'role'],
+        })
+        res.status(200).json({
+          status: 200,
+          message: 'account created successfully',
+          token: encode({id: createData.id, email: createData.email}),
+        })
+      }
+    } catch (error: any) {
+      res.status(500).json({
+        status: 500,
+        message: 'Server error :' + error.message,
+      })
     }
-    static async getAlluser(req: Request, res: Response) {
-        try {
-            const users: object = await USER.findAll({ attributes: { exclude: ['password'] } })
-            res.status(200).json({
-                statuscode: 200,
-                users
-            })
-        } catch (error: any) {
-            res.status(400).json({
-                statusCode: 400,
-                "message": error.message
-            })
-        }
-    }
-    /*this delete user function is not protected it is created just for the project setup and testing*/
-    static async deleteUser(req: Request, res: Response) {
-        const userid: string = req.params.id
-        try {
-            await USER.destroy({ where: { id: userid } })
-            res.status(200).json({
-                statusCode: 200,
-                "message": `deleted user with id ${userid}`
-            })
+  }
 
-        } catch (error: any) {
-            res.status(400).json({
-                statusCode: 400,
-                "message": error.message
-            })
-
-
-        }
+  static async getAlluser(req: Request, res: Response) {
+    try {
+      const users: object = await USER.findAll({
+        attributes: {exclude: ['password']},
+      })
+      res.status(200).json({
+        statuscode: 200,
+        users,
+      })
+    } catch (error: any) {
+      res.status(400).json({
+        statusCode: 400,
+        message: error.message,
+      })
     }
-
-    static sendCode(req: Request, res: Response) {
-        const userPhone: string = req.params.phone;
-        if (account_sid && authToken && service_sid) {
-            const Client = new Twilio(account_sid, authToken);
-            Client.verify.v2.services(service_sid)
-                .verifications
-                .create({ to: userPhone, channel: 'sms' })
-                .then(resp => {
-                    res.status(200).json({message: "Success sent", resp})
-                })
-                .catch(err => {
-                    res.status(400).json(err)
-                })
-        } else {
-            console.log('Please fill all vals')
-        }
+  }
+  /*this delete user function is not protected it is created just for the project setup and testing*/
+  static async deleteUser(req: Request, res: Response) {
+    const userid: string = req.params.id
+    try {
+      await USER.destroy({where: {id: userid}})
+      res.status(200).json({
+        statusCode: 200,
+        message: `deleted user with id ${userid}`,
+      })
+    } catch (error: any) {
+      res.status(400).json({
+        statusCode: 400,
+        message: error.message,
+      })
     }
-    static verify2FA(req: Request, res:Response) {
-        const userPhone: string = req.params.phone;
-        const userCode: string = req.params.code;
-        if (account_sid && authToken && service_sid) {
-            const Client = new Twilio(account_sid, authToken);
-            Client.verify.v2.services(service_sid)
-                .verificationChecks
-                .create({ to: userPhone, code: userCode })
-                .then(resp => {
-                    res.status(200).json({message: "Success sent", resp})
-                })
-                .catch(err => {
-                    res.status(400).json(err)
-                })
-        } else {
-            console.log('Please fill all vals')
-        }
-    }
+  }
 }
 export default auth
--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -1,14 +1,48 @@
 import {Router} from "express"
 import auth from "../controllers/authController"
+import signupValidation from "../middelwares/signupValidation"
 
 const router = Router()
 
-
-router.post('/signup', auth.signup)
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     tags:
+ *       - users
+ *     summary: all Users
+ *     security: []
+ *     requestBody:
+ *        required: true
+ *        content:
+ *           application/json:
+ *              schema:
+ *                 type: object
+ *                 required:                    
+ *                    - email
+ *                    - firstName
+ *                    - lastName
+ *                    - password                   
+ *                 properties:                 
+ *                    email:
+ *                      type: string
+ *                    firstname:
+ *                      type: string
+ *                    lastName:
+ *                       type: string
+ *                    password: 
+ *                       type : string                   
+ *     responses:
+ *       201:
+ *             description: successfully logged in;
+ *       
+ * */
+router.post('/signup', signupValidation, auth.signup)
 router.get('/users', auth.getAlluser)
 router.delete('/delete/:id', auth.deleteUser)
 router.get('/sendcode/:phone', auth.sendCode)
 router.get('/verify/:phone/:code', auth.verify2FA)
+router.get('/logout', auth.logout)
 /* this delete user route is not protected it is just for testing and setting up the project*/
 
-export default router 
+export default router
